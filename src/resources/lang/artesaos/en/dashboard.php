800f284fe33a42098373d00ce49a97b5c354dba9
Vagner do Carmo <vluzrmos@gmail.com>
2015-06-25 02:06:14 -0300

    move languages pt to pt_BR and add the readme

---
+++ src/resources/lang/artesaos/en/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'users' => [
+
+        'index' => [
+
+            'heading' => 'Registred Users'
+
+        ],
+
+        'show' => [
+
+            'heading' => 'User Details'  
+
+        ]
+
+    ],
+
+    'navigation' => [
+
+        'brand'       => 'Defender Dashboard',
+
+        'users'       => 'Users',
+        
+        'roles'       => 'Roles',
+        
+        'permissions' => 'Permissions'
+
+    ]
+
+];
