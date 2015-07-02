49fb04ef974faae13c13d5c45797f17c6b2331db
emtudo <emtudo@gmail.com>
2015-07-02 00:44:58 -0300

    Protection of the dashboard

--- src/Dashboard/Providers/DashboardServiceProvider.php
+++ src/Dashboard/Providers/DashboardServiceProvider.php
@@ -69,7 +69,9 @@ class DashboardServiceProvider extends ServiceProvider
 
         $prefix = $this->app['config']->get('dashboard.route_prefix', 'dashboard');
 
-        $router->group(['prefix' => $prefix, 'middleware' => ['auth','needsPermission'], 'can' => config('dashboard.permission_name')], function () use ($router) {
+        $permission = $this->app['config']->get('dashboard.permission_name');
+
+        $router->group(['prefix' => $prefix, 'middleware' => ['auth','needsPermission'], 'can' => $permission], function () use ($router) {
             require __DIR__.'/../../resources/routes.php';
         });
     }
